c7a848abc6534bce63e9007e05372ff6735a6a68
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-10-31 09:12:29 +0700

    Add files via upload

---
+++ PBO10119012Latihan18.java
@@ -0,0 +1,18 @@
+package pbo10119012latihan18;
+
+/**
+ *
+ * @author damai
+ * NAMA     : Damai Saputra Laoli
+ * KELAS    : IF 1
+ * NIM      : 10119012
+ */
+public class PBO10119012Latihan18 {
+
+    
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println("i : "+i);
+        }
+    }
+}
